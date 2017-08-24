31e65f1cd6801a29deb0ede31ff624d178a1acc1
Eric Rouleau <erouleau@cloud.ca>
2017-08-24 16:23:35 -0400

    added requirements and keywords in setup

--- setup.py
+++ setup.py
@@ -4,8 +4,12 @@ setup(name='opentsdb_rest_client',
       version='0.1',
       description='Python client for the OpenTSDB HTTP REST API',
       url='http://github.com/xblitz/opentsdb_rest_client',
+      keywords = ["opentsdb","api","client"],
       author='Eric Rouleau',
       author_email='xblitz@gmail.com',
       license='MIT',
       packages=['opentsdb_rest_client'],
+      install_requires = [
+            "requests >= 2.0.0",
+      ],
       zip_safe=False)
