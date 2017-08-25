08200a404031042346e71d15a4b6e1cdf29f9e88
Eric Rouleau <erouleau@cloud.ca>
2017-08-25 10:42:24 -0400

    cosmetics fix of setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup
 
-setup(name='opentsdb_rest_client',
-      version='0.1',
-      description='Python client for the OpenTSDB HTTP REST API',
-      url='http://github.com/xblitz/opentsdb_rest_client',
-      keywords = ["opentsdb","api","client"],
-      author='Eric Rouleau',
-      author_email='xblitz@gmail.com',
-      license='MIT',
-      packages=['opentsdb_rest_client'],
-      install_requires = [
-            "requests >= 2.0.0",
-      ],
-      zip_safe=False)
+setup(
+    name='opentsdb_rest_client',
+    version='0.1',
+    description='Python client for the OpenTSDB HTTP REST API',
+    url='http://github.com/xblitz/opentsdb_rest_client',
+    keywords=["opentsdb", "api", "client"],
+    author='Eric Rouleau',
+    author_email='xblitz@gmail.com',
+    license='MIT',
+    packages=['opentsdb_rest_client'],
+    install_requires=[
+        "requests >= 2.0.0",
+    ],
+    zip_safe=False
+)
